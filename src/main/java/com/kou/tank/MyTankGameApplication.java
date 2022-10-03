ff26a8d3b21f511239d320cd04292e204ea810aa
jiajun <soslovemiss@dingtalk.com>
2022-10-04 07:47:53 +0800

    Initial commit

---
+++ src/main/java/com/kou/tank/MyTankGameApplication.java
@@ -0,0 +1,13 @@
+package com.kou.tank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTankGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTankGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kou/tank/MyTankGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kou.tank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTankGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
